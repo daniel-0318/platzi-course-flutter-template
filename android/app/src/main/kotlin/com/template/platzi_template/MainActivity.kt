08a865fd4bbcdcc992fb2d9445b677edbfcc57a8
Daniel Ramirez <dani_0318@hotmail.com>
2022-02-04 23:07:08 -0500

    Description added

---
+++ android/app/src/main/kotlin/com/template/platzi_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.template.platzi_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/description_place.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Description_place extends StatelessWidget{
+
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  Description_place(this.namePlace, this.stars, this.descriptionPlace);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final star_half = Container(
+      margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star_border= Container(
+      margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+
+      child: Icon(
+        Icons.star,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final title_stars = Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0
+          ),
+          child: Text(
+            namePlace,
+            style: TextStyle(
+              fontSize:  30.0,
+              fontFamily: "Lato",
+              fontWeight: FontWeight.w900
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            star,
+            star,
+            star,
+            star_half,
+            star_border
+          ],
+        )
+      ],
+    );
+    
+    final description = Container(
+      margin: EdgeInsets.only(
+        top: 10.0,
+        left: 20.0,
+        right: 20.0
+      ),
+      child: Text(descriptionPlace,
+        style: TextStyle(
+          fontFamily: "Lato",
+            fontSize: 14.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF56575a)
+        ),
+        textAlign: TextAlign.justify,
+      ),
+    );
+
+    final content = Column(
+      children:<Widget> [
+        title_stars,
+        description
+      ],
+    );
+
+    return content;
+  }
+  
+}
